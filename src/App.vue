737636398f4ee5b8c8842768446dd3f91287dfa6
Aleksey Konyakhin <konyaxin.1998@gmail.com>
2025-04-13 21:34:23 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div className="app">
+        <!-- <AppInfo /> -->
+
+        <div className="search-panel">
+            <!-- <SearchPanel />
+            <AppFilter /> -->
+        </div>
+
+        <!-- <EmployeesList />
+        <EmployeesAddForm /> -->
+    </div>
+</template>
+
+<style scoped></style>
